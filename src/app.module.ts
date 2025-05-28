1ba74547ec559888608aad0a34dfce2f83c74cce
jaytsol <jaytsol@naver.com>
2025-05-28 20:11:47 +0900

    added minor

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ import { AuthModule } from './auth/auth.module';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
+      host: process.env.DB_HOST || 'localhost',
       port: parseInt(process.env.DB_PORT || '5432', 10),
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
