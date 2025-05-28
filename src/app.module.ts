b5dc70f56d1ed61507ab5b5f527612b3b42d6a05
jaytsol <jaytsol@naver.com>
2025-05-28 16:17:32 +0900

    postgras added

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,9 @@ import { ConfigModule } from '@nestjs/config';
 import { ComfyuiController } from './comfyui/comfyui.controller';
 import { ComfyuiService } from './comfyui/comfyui.service';
 import { ComfyuiModule } from './comfyui/comfyui.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './auth/entity';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -12,6 +15,18 @@ import { ComfyuiModule } from './comfyui/comfyui.module';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [User],
+      synchronize: true,
+      logging: true,
+    }),
+    AuthModule,
     ComfyuiModule,
   ],
   controllers: [AppController, ComfyuiController],
