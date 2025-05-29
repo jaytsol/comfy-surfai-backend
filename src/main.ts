984e4b9a856070cbe675ad3c8f1ebe9bd3967e0b
jaytsol <jaytsol@naver.com>
2025-05-29 18:26:31 +0900

    added configService & session

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,51 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import session from 'express-session';
+import connectPgSimple from 'connect-pg-simple';
+import passport from 'passport';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  const configService = app.get(ConfigService);
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const PGStore = connectPgSimple(session);
+  const sessionStore = new PGStore({
+    conString: configService.get<string>('DATABASE_URL'),
+    tableName: 'sessions',
+    createTableIfMissing: true,
+  });
+
+  app.use(
+    session({
+      store: sessionStore,
+      secret: configService.get<string>('SESSION_SECRET') ?? '',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24, // 1 day
+        httpOnly: true,
+        // secure: configService.get<string>('NODE_ENV === 'production', // HTTPS 환경에서만
+      },
+    }),
+  );
+
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  await app.listen(configService.get<number>('PORT') ?? 3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
