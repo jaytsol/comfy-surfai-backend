6ad8250a859dd23ea01484cc08d51bc4d5583fae
jaytsol <jaytsol@naver.com>
2025-05-29 18:36:55 +0900

    eslint configure changed

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
