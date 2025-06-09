c7cad49b227f6642a0ab9fc99d2e36b4ddd12fb7
jaytsol <jaytsol@naver.com>
2025-06-09 19:44:55 +0900

    added R2 storage

--- src/storage/storage.module.ts
+++ src/storage/storage.module.ts
@@ -2,9 +2,11 @@ import { Logger, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { storageConfig } from './constants/storage.constants';
 import { CloudflareR2Service } from './services/cloudflare-r2.service';
+import { StorageController } from './storage.controller';
 
 @Module({
   imports: [ConfigModule.forFeature(storageConfig)],
+  controllers: [StorageController],
   providers: [
     {
       provide: 'IStorageService',
