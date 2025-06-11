c1a8fd1599d4e8eec1bf7b1dd55e696c36b83e7a
jaytsol <jaytsol@naver.com>
2025-06-12 03:11:48 +0900

    import module

--- src/generated-output/generated-output.module.ts
+++ src/generated-output/generated-output.module.ts
@@ -2,14 +2,13 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { GeneratedOutput } from '../common/entities/generated-output.entity';
 import { GeneratedOutputService } from './generated-output.service';
-// import { GeneratedOutputController } from './generated-output.controller'; // 향후 히스토리 API 컨트롤러 추가 시
+import { AuthModule } from '../auth/auth.module';
+import { GeneratedOutputController } from './generated-output.controller';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([GeneratedOutput]), // 이 모듈에서 GeneratedOutput 리포지토리를 사용하도록 설정
-  ],
-  // controllers: [GeneratedOutputController],
+  imports: [TypeOrmModule.forFeature([GeneratedOutput]), AuthModule],
+  controllers: [GeneratedOutputController],
   providers: [GeneratedOutputService],
-  exports: [GeneratedOutputService], // ComfyuiModule에서 이 서비스를 주입받아 사용해야 하므로 export
+  exports: [GeneratedOutputService],
 })
 export class GeneratedOutputModule {}
