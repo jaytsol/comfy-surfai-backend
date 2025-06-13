7a3c0447ca6d63581bc4073366afa4639f378052
jaytsol <jaytsol@naver.com>
2025-06-13 17:36:45 +0900

    resolved dependency issue

--- src/generated-output/generated-output.module.ts
+++ src/generated-output/generated-output.module.ts
@@ -4,9 +4,14 @@ import { GeneratedOutput } from '../common/entities/generated-output.entity';
 import { GeneratedOutputService } from './generated-output.service';
 import { AuthModule } from '../auth/auth.module';
 import { GeneratedOutputController } from './generated-output.controller';
+import { StorageModule } from 'src/storage/storage.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([GeneratedOutput]), AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([GeneratedOutput]),
+    AuthModule,
+    StorageModule,
+  ],
   controllers: [GeneratedOutputController],
   providers: [GeneratedOutputService],
   exports: [GeneratedOutputService],
