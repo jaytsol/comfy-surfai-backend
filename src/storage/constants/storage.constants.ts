910e5e4a6e7002df559c6463650685168a31df3c
jaytsol <jaytsol@naver.com>
2025-06-18 17:09:26 +0900

    chore

--- src/storage/constants/storage.constants.ts
+++ src/storage/constants/storage.constants.ts
@@ -15,7 +15,6 @@ export const storageConfig = registerAs(STORAGE_CONFIG, () => ({
 
 export const R2_CONFIG = {
   REGION: 'auto',
-  // 운영 환경에서는 https, 개발 환경에서는 http를 사용하도록 수정
   ENDPOINT: (accountId: string): string =>
     `https://${accountId}.r2.cloudflarestorage.com`,
   PUBLIC_URL: (bucketName: string, accountId: string): string =>
