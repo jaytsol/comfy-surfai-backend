b3751228b768a7e10f9dc63cd2a9446cf80b8fd9
jaytsol <jaytsol@naver.com>
2025-08-20 02:41:38 +0900

    google-youtube strategy file updated

---
+++ src/modules/social/strategies/google-youtube.strategy.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+
+@Injectable()
+export class GoogleYouTubeStrategy extends PassportStrategy(
+  Strategy,
+  'google-youtube',
+) {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      clientID: configService.get('GOOGLE_CLIENT_ID'),
+      clientSecret: configService.get('GOOGLE_CLIENT_SECRET'),
+      callbackURL: configService.get('GOOGLE_YOUTUBE_CALLBACK_URL'),
+      scope: [
+        'email',
+        'profile',
+        'https://www.googleapis.com/auth/youtube.upload',
+      ],
+      accessType: 'offline', // To get a refresh token
+      prompt: 'consent', // To ensure a refresh token is provided every time
+    });
+  }
+
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: VerifyCallback,
+  ): Promise<any> {
+    const { name, emails, photos } = profile;
+    const user = {
+      email: emails[0].value,
+      firstName: name.givenName,
+      lastName: name.familyName,
+      picture: photos[0].value,
+      accessToken,
+      refreshToken,
+    };
+    done(null, user);
+  }
+}
