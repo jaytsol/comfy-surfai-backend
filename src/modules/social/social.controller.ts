749aeffc841e1d84665d6b06d70f50d0eaf2d6df
jaytsol <jaytsol@naver.com>
2025-08-20 02:45:33 +0900

    controller updated

--- src/modules/social/social.controller.ts
+++ src/modules/social/social.controller.ts
@@ -1,12 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { SocialService } from './social.service';
 
-@Controller('social')
+@Controller('connect')
 export class SocialController {
   constructor(private readonly socialService: SocialService) {}
 
+  @Get('google')
+  @UseGuards(AuthGuard('google-youtube'))
+  async connectGoogle(@Req() req) {
+    // This route will redirect to Google for authentication
+  }
+
+  @Get('google/callback')
+  @UseGuards(AuthGuard('google-youtube'))
+  async googleCallback(@Req() req) {
+    // The AuthGuard will handle the logic, and the user object will be on req.user
+    // Next, we will pass this to the service to save the connection details.
+    // For now, we can just return a success message.
+    return { message: 'Google account connected successfully' };
+  }
+
   // TODO: Implement GET /connections
-  // TODO: Implement GET /connect/:platform
-  // TODO: Implement GET /connect/:platform/callback
   // TODO: Implement POST /disconnect/:platform
 }
--- src/modules/social/social.module.ts
+++ src/modules/social/social.module.ts
@@ -3,10 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SocialConnection } from './entities/social-connection.entity';
 import { SocialController } from './social.controller';
 import { SocialService } from './social.service';
+import { GoogleYouTubeStrategy } from './strategies/google-youtube.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SocialConnection])],
+  imports: [TypeOrmModule.forFeature([SocialConnection]), PassportModule],
   controllers: [SocialController],
-  providers: [SocialService],
+  providers: [SocialService, GoogleYouTubeStrategy],
 })
 export class SocialModule {}
